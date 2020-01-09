e746313eed1ea89e2a6300680775abdf800177d6
barlinly@gmail.com <barlinly@gmail.com>
2020-01-09 13:55:10 +0200

    blabla

--- book.php
+++ book.php
@@ -1,8 +1,10 @@
 <?php
+
 require_once 'db_connection.php';
+
 $id = $_GET['id'];
-$title = $_GET['title'];
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmt = $pdo->prepare('SELECT * FROM Books.books b LEFT JOIN book_authors ba ON ba.book_id = b.id
+LEFT JOIN authors a ON ba.author_id = a.id WHERE b.id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 ?>
@@ -15,6 +17,36 @@ $book = $stmt->fetch();
     <title><?= $book['title']; ?></title>
 </head>
 <body>
-    
+<img src="<?php echo $book['cover_path']; ?>" alt="Smiley face" height="200" width="200">
+<br> 
+<h3>Raamatu pealkiri</h3>
+<?php echo $book['title']; ?>  
+<br>
+<h3>Raamatu autor</h3>
+<?php echo $book['first_name']; ?> <?php echo $book['last_name']; ?>
+<h3>Raamatu ilmumisaasta</h3>
+<?php echo $book['release_date']; ?>  
+<br>
+<h3>Raamatu keel</h3>
+<?php echo $book['language']; ?>  
+<br>
+<h3>Raamatu kirjeldus</h3>
+<?php echo $book['summary']; ?>  
+<br>
+<h3>Raamatu hind</h3>
+<?php echo $book['price']; ?>
+<br> 
+<h3>Raamatu lehekülgede arv</h3> 
+<?php echo $book['pages']; ?> 
+<br>
+<h3>Saadavus</h3> 
+<?php if ($book['stock_saldo'] > 0 ) {
+    echo "On saadaval";
+} else {
+    echo "Pole saadaval";
+}     
+?>  
+
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
-
-$host = 'localhost';
-$db   = 'Books';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
+require_once 'db_connection.php';
 // var_dump($_GET);
 $title = $_GET['title'];
 $year = $_GET['year'];
